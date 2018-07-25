686a6d1199bf7dd5705dac5e92b456f82195a91d
Karen Kraft <kraftkj@comcast.net>
2018-07-25 14:27:47 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -5,10 +5,10 @@ def first_student_by_index
   puts STUDENT_NAMES.first
 end
 
-first_student_by_index
+#first_student_by_index
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES[4]
 end
 
 def last_student_by_index

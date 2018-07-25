aee5bcff941352d8cbfe4fb7277e98314206ccc3
Karen Kraft <kraftkj@comcast.net>
2018-07-25 14:18:28 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,16 +1,18 @@
 
-# def first_student_by_index
-#   # Write a solution that returns the first student in the array student_names
-#   STUDENT_NAMES = [
-#   "Adele",
-#   "Beyoncé",
-#   "Cardi B",
-#   "Lady Gaga",
-#   "Nicki Minaj",
-#   "Rihanna"
-# ]
-  puts  = "Adele"
+def first_student_by_index
+  # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES = [
+   "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
+  puts STUDENT_NAMES= "Adele"
 end
+
+first_student_by_index
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names

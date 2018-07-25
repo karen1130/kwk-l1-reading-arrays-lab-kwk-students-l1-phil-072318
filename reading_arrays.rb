312a6bc33f6085ad423c3afc1491c0dca6061a08
Karen Kraft <kraftkj@comcast.net>
2018-07-25 14:29:56 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,11 +1,11 @@
 STUDENT_NAMES =[ "Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna" ]
 
-# def first_student_by_index
-#   # Write a solution that returns the first student in the array student_names
-#   puts STUDENT_NAMES.first
-# end
+def first_student_by_index
+  # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES.first
+end
 
-# #first_student_by_index
+#first_student_by_index
 
 def fourth_student_by_index
   puts STUDENT_NAMES[3]
@@ -13,8 +13,9 @@ end
 fourth_student_by_index
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES.last
 end
+last_student_by_index
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method

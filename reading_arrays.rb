b35661690bfc17e81f61c8e4beddf007f17dc454
Karen Kraft <kraftkj@comcast.net>
2018-07-25 14:20:01 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,7 +1,7 @@
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
-  STUDENT_NAMES = ["Adele", "Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+  STUDENT_NAMES = ["Beyoncé", "Cardi B", "Lady Gaga", "Nicki Minaj", "Rihanna"]
   puts STUDENT_NAMES[0] = "Adele"
 end
 
